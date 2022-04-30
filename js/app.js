e2b88d43a94b9b8bddbc0bc66c4ccfb0058d0f1f
Nəzrin <isbndyrva@gmail.com>
2022-04-30 18:29:39 +0400

    Update landing page

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 // loader
+$("body").css(
+  "overflow-y",
+  "hidden",
+  setTimeout(() => {
+    $("body").css("overflow-y", "visible");
+  }, 2000)
+);
+
+$(window).on("load", () => {
+  setTimeout(removeLoader, 1700);
+});
+
 function removeLoader() {
   $("#loadingDiv").fadeOut(500, () => {
     $("#loadingDiv").remove();
   });
 }
-
-$(window).on("load", () => {
-  setTimeout(removeLoader, 2000);
-
-  $("body").css(
-    "overflow-y",
-    "hidden",
-    setTimeout(() => {
-      $("body").css("overflow-y", "visible");
-    }, 2000)
-  );
-});
